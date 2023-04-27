1a2ad435266091a3af112b742d38306e556d24fd
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2023-04-27 23:50:18 +0900

    chore: mod cypress config setupNodeEvents

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      console.log(config) // see everything in here!
+
+      // modifying config value with config object
+
+      // if modified, IMPORTANT to return the updated config object
+      return config
     },
   },
-});
+})
